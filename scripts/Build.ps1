857aac2612772522cd38eedbbaebd26fe473b160
S.C.H. Study <siuchunhung@gmail.com>
2025-01-27 08:27:00 +0800

    Update Build.ps1

--- scripts/Build.ps1
+++ scripts/Build.ps1
@@ -85,7 +85,7 @@ if ($NativeExecutable)
     else { throw 'unknown os' }
 
     $OSArchitecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
-    $Arch = if ($OSArchitecture -eq 'Arm64') { 'aarch64' }
+    $Arch = if ($OSArchitecture -eq 'X64') { 'aarch64' }
     elseif ($OSArchitecture -eq 'Arm64') { 'aarch64' }
     else { throw 'unkown arch' }
 
